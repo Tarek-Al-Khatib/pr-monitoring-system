573f689b068b2550728cff51f70812ae67fbc4d5
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-28 22:13:45 +0200

    Added getOldRequests function to PullRequestActionsController

--- app/Http/Controllers/PullRequestsActionsController.php
+++ app/Http/Controllers/PullRequestsActionsController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Http;
 use Illuminate\Http\Request;
 
 class PullRequestsActionsController extends Controller
 {
-    //
+    private $repoUrl = "https://api.github.com/repos/woocommerce/woocommerce/pulls";
+
+    public function getOldRequests() {
+        try {
+            $response = Http::get($this->repoUrl);
+            $data = $response->json();
+
+            $oldPRs = array_filter($data, function($pr){
+                return now()->diffInDays($pr['created_at'] > 14);
+            });
+
+            return response()->json($oldPRs);
+            
+        }catch(\Exception $e){
+            return response()->json(["message"=>"Error while fetching old messages", "error"=>$e->getMessage()]);
+        }
+    }
 }
