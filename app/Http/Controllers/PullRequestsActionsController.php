7c0be3402d0db40722c54032254ca1fa25a09520
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-29 15:10:01 +0200

    Fixed the pagination bug for old requests in addition to date comparison bug

--- app/Http/Controllers/PullRequestsActionsController.php
+++ app/Http/Controllers/PullRequestsActionsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Http;
 use Illuminate\Http\Request;
 
@@ -11,17 +12,41 @@ class PullRequestsActionsController extends Controller
 
     public function getOldRequests() {
         try {
-            $response = Http::get($this->repoUrl);
-            $data = $response->json();
+            $page = 1;
+            $data = [];
+            $cutoffTimestamp = strtotime(gmdate('Y-m-d H:i:s')) - (14 * 24 * 60 * 60);
 
-            $oldPRs = array_filter($data, function($pr){
-                return now()->diffInDays($pr['created_at'] > 14);
-            });
-
-            return response()->json($oldPRs);
+            do {
+                $response = Http::withHeaders([
+                    'Authorization' => 'Bearer ' . env('GITHUB_ACCESS_TOKEN'),
+                    'Accept' => 'application/vnd.github.v3+json'
+                ])->get($this->repoUrl, [
+                    'per_page' => 30,
+                    'page' => $page
+                ]);
+                $currentPageData = $response->json();
+    
+                $data = array_merge($data, $currentPageData);
+                $page++;
+            } while (!empty($currentPageData));
             
-        }catch(\Exception $e){
-            return response()->json(["message"=>"Error while fetching old messages", "error"=>$e->getMessage()]);
+            $oldPRs = array_filter($data, function ($pr) use ($cutoffTimestamp) {
+                if (!isset($pr['created_at'])) {
+                    return false;
+                }
+                
+                $createdTimestamp = strtotime($pr['created_at']);
+                if ($createdTimestamp === false) {
+                    return false;
+                }
+                
+                return $createdTimestamp < $cutoffTimestamp;
+            });
+    
+            return response()->json(array_values($oldPRs));
+        } catch (\Exception $e) {
+            return response()->json(["message" => "Error while fetching old pull requests", "error" => $e->getMessage()]);
         }
     }
+    
 }
