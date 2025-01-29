409bfac825cd7387e5803583f5ecaa7844afcbba
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-29 15:09:22 +0200

    Added old requests route to api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\PullRequestsActionsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix("pull-requests")->group(function(){
+    Route::get("/old", [PullRequestsActionsController::class,"getOldRequests"]);
+});
