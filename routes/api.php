49549d6bca482325e5fdb8514e45afe4f5cde1a4
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-29 16:11:29 +0200

    Routed the review success function in api.php

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::prefix("pull-requests")->group(function(){
     Route::get("/old", [PullRequestsActionsController::class,"getOldRequests"]);
     Route::get("/requied-review", [PullRequestsActionsController::class, "getReviewRequiredRequests"]);
+    Route::get("/successful-review", [PullRequestsActionsController::class, "getSuccessfulReview"]);
 });
