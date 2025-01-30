95e2bff6494e0e537e657529a50d67bc143cf32e
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2025-01-30 03:05:00 +0200

    Added spreadsheet saving to function and integrated it

--- app/Http/Controllers/PullRequestsActionsController.php
+++ app/Http/Controllers/PullRequestsActionsController.php
@@ -48,29 +48,46 @@ class PullRequestsActionsController extends Controller
         return $data;
     }
 
+    private function saveToSpreadSheet($sheetname, $data){
+        try{        
+            $sheet = Sheets::spreadsheet(env("SPREADSHEET_ID"))->sheet($sheetname)->clear();
+            $appendableData = [["PR#", "PR Title", "URL"]];
+            foreach($data as $pr){
+                $appendableData[] = [$pr["number"], $pr["title"], $pr["url"]];
+            }
+            $sheet = Sheets::spreadsheet(env("SPREADSHEET_ID"))->sheet($sheetname)->append($appendableData);
+            $sheet = Sheets::spreadsheet(env("SPREADSHEET_ID"))->sheet($sheetname)->get();
+            $header = $sheet->pull(0);
+            $values = Sheets::collection($header, $sheet);
+            $data = array_values($values->toArray());
+            return true;
+        }catch(\Exception $e) {
+            return "Error saving to spreadsheet" . $e->__tostring();
+        }
+    }
     private function saveToFile($filename, $data)
     {
-        $filePath = "github_reports/" . $filename;
-        $content = "Total Pull Requests: " . count($data) . "\n\n";
+        try{      
+            $filePath = "github_reports/" . $filename;
+            $content = "Total Pull Requests: " . count($data) . "\n\n";
 
-        foreach ($data as $pr) {
-            $content .= "* PR# " . $pr["number"] . " - " . $pr["title"] . " - " . "URL: " . $pr["html_url"] . "\n";
+            foreach ($data as $pr) {
+                $content .= "* PR# " . $pr["number"] . " - " . $pr["title"] . " - " . "URL: " . $pr["html_url"] . "\n";
+            }
+
+            Storage::put($filePath, $content);
+            return "File saved: " . $filePath;
+        }catch(\Exception $e) {
+            return "Error saving to file" . $e->__tostring();
         }
-
-        Storage::put($filePath, $content);
-        return response()->json(["message" => "File saved: " . $filePath]);
     }
     public function getOldRequests() {
-        // $sheet = Sheets::spreadsheet("1fp2OdjbhVw_Y03mxB9nBvb478CR1wFd9wQHAyLAxh1U")->sheet("old")->get();
-        // $header = $sheet->pull(0);
-        // $values = Sheets::collection($header, $sheet);
-        // $values->toArray();
-
-        // dd($values);
         try {
             $query = "repo:woocommerce/woocommerce type:pr is:open created:<" . now()->subDays(14)->format("Y-m-d");
             $data = $this->loopThroughResponse($query, $this->issuesULR);
-            return $this->saveToFile("1-old-pull-requests.txt", $data);
+            $fileSavingResponse = $this->saveToFile("1-old-pull-requests.txt", $data);
+            $spreadsheetSavingResponse = $this->saveToSpreadSheet("old", $data);
+            return response()->json(["file-saving"=> $fileSavingResponse, "spreadsheetSaving"=> $spreadsheetSavingResponse]);
         } catch (\Exception $e) {
             return response()->json(["message" => "Error while fetching old pull requests", "error" => $e->getMessage()]);
         }
@@ -81,7 +98,9 @@ class PullRequestsActionsController extends Controller
         try {
             $query = "repo:woocommerce/woocommerce type:pr is:open review:required";;
             $data = $this->loopThroughResponse($query, $this->issuesULR);
-            return $this->saveToFile("2-review-required-pull-requests.txt", $data);
+            $fileSavingResponse = $this->saveToFile("2-review-required-pull-requests.txt", $data);
+            $spreadsheetSavingResponse = $this->saveToSpreadSheet("review-required", $data);
+            return response()->json(["file-saving"=> $fileSavingResponse, "spreadsheetSaving"=> $spreadsheetSavingResponse]);
         } catch (\Exception $e) {
             return response()->json(["message" => "Error fetching pull requests requiring review", "error" => $e->getMessage()]);
         }
@@ -91,7 +110,9 @@ class PullRequestsActionsController extends Controller
         try {
             $query = "repo:woocommerce/woocommerce type:pr is:open status:success";
             $data = $this->loopThroughResponse($query, $this->issuesULR);
-            return $this->saveToFile("3-successful-review-pull-requests.txt", $data);
+            $fileSavingResponse = $this->saveToFile("3-successful-review-pull-requests.txt", $data);
+            $spreadsheetSavingResponse = $this->saveToSpreadSheet("successful-review", $data);
+            return response()->json(["file-saving"=> $fileSavingResponse, "spreadsheetSaving"=> $spreadsheetSavingResponse]);
         } catch (\Exception $e) {
             return response()->json(["message" => "Error fetching pull requests with successful review", "error" => $e->getMessage()]);
         }
@@ -104,7 +125,9 @@ class PullRequestsActionsController extends Controller
             $unassignedPRs = array_filter($data, function ($pr) {
                 return empty($pr['requested_reviewers']) && empty($pr['requested_teams']);
             });
-            return $this->saveToFile("4-no-reviewer-pull-requests.txt", $unassignedPRs);
+            $fileSavingResponse = $this->saveToFile("4-no-reviewer-pull-requests.txt", $unassignedPRs);
+            $spreadsheetSavingResponse = $this->saveToSpreadSheet("no-review", $unassignedPRs);
+            return response()->json(["file-saving"=> $fileSavingResponse, "spreadsheetSaving"=> $spreadsheetSavingResponse]);
         } catch (\Exception $e) {
             return response()->json(["message" => "Error fetching pull requests with no reviews assigned", "error" => $e->getMessage()]);
         }
